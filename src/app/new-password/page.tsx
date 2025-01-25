d0414066a775375c118671322ffcbda1f975a070
diegoTech14 <diegotech1499@gmail.com>
2025-01-25 16:10:50 -0600

    Feat: added the lateral images

--- src/app/new-password/page.tsx
+++ src/app/new-password/page.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import InputField from '@/components/Form/InputField';
 import Button from '@/components/Form/Button';
+import Image from 'next/image';
 
 export default function PasswordChangeForm() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white">
-      <div className="w-full max-w-md p-8">
+    <div className="min-h-screen flex items-center justify-center bg-white relative">
+      <div className="absolute top-0 left-0 h-full w-1/4">
+        <Image
+          src="/images/left.png"
+          alt="left-side"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+
+      <div className="relative z-10 w-full max-w-md p-8 mx-auto bg-white">
         <h1 className="text-3xl font-bold text-center mb-2">New password</h1>
         <p className="text-gray-600 text-center mb-8">Please, change the password</p>
 
@@ -29,6 +39,15 @@ export default function PasswordChangeForm() {
           </div>
         </form>
       </div>
+      
+      <div className="absolute top-0 right-0 h-full w-1/4">
+        <Image
+          src="/images/right.png"
+          alt="right-side"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
     </div>
   );
-};
+}
