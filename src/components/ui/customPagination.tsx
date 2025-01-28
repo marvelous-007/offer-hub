224e945edf6939edd2f0a42560d1e6463f97469f
CristopherAguilar10 <jankaguilar1030@gmail.com>
2025-01-27 23:33:42 -0600

    feat: removing comments

--- src/components/ui/customPagination.tsx
+++ src/components/ui/customPagination.tsx
@@ -22,14 +22,14 @@ export default function CustomPagination({
   totalPages,
   onPageChange,
 }: PaginationProps) {
-  const range = 2; // Number of pages to show around the current page
+  const range = 2; 
   const pages = [];
 
   for (let i = 1; i <= totalPages; i++) {
     if (
-      i === 1 || // First page
-      i === totalPages || // Last page
-      (i >= currentPage - range && i <= currentPage + range) // Pages around the current
+      i === 1 || 
+      i === totalPages || 
+      (i >= currentPage - range && i <= currentPage + range) 
     ) {
       pages.push(i);
     } else if (pages[pages.length - 1] !== "...") {
@@ -39,7 +39,7 @@ export default function CustomPagination({
 
   return (
     <Pagination className="flex items-center gap-2 justify-center">
-      {/* Previous Button */}
+      
       <PaginationPrevious
         onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
         className={currentPage === 1 ? "text-gray-400 cursor-not-allowed" : "text-[#002333]"}
@@ -47,7 +47,7 @@ export default function CustomPagination({
         Previous
       </PaginationPrevious>
 
-      {/* Page Numbers */}
+      
       <PaginationContent>
         {pages.map((page, index) => (
           <React.Fragment key={index}>
@@ -68,7 +68,7 @@ export default function CustomPagination({
         ))}
       </PaginationContent>
 
-      {/* Next Button */}
+      
       <PaginationNext
         onClick={() => currentPage < totalPages && onPageChange(currentPage + 1)}
         className={currentPage === totalPages ? "text-gray-400 cursor-not-allowed" : "text-[#002333]"}
