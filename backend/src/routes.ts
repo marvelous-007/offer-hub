ee8bbbd0ce4ed30172366432418e2f0269a21f05
Electrifier <samchijioke16@gmail.com>
2025-02-22 00:41:14 +0100

    fix: resolve merge conflict in `src/routes.ts` file

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import notificationsController from "./controllers/notifications.controller";
+import authLogsController from "./controllers/auth-logs.controller";
 import { activityLogsRouter } from "@/controllers";
 
 const router = Router();
 router.use("/notifications", notificationsController);
+router.use("/auth-logs", authLogsController);
 router.use("/activity-logs", activityLogsRouter);
 
 export default router;
