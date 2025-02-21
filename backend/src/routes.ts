f830604a4b8da553faa83209b3ecbadf72f0873f
Daniel Coto Jimenez <dannyc0902@gmail.com>
2025-02-21 12:35:21 -0600

    feat: auth logs

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import notificationsController from "./controllers/notifications.controller";
+import authLogsController from "./controllers/auth-logs.controller";
 
 const router = Router();
+
 router.use("/notifications", notificationsController);
+router.use("/auth-logs", authLogsController);
 
 export default router;
