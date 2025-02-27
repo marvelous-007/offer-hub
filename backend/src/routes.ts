9ffee0b0892d4d50e7aa8c0f08cd95c1db3140f3
jimenezz22 <ljimenezbogantes@gmail.com>
2025-02-26 18:27:33 -0600

    feat: add route for users management using UsersController

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,6 +5,7 @@ import notificationsController from "./controllers/notifications.controller";
 import authLogsController from "./controllers/auth-logs.controller";
 import certificationsController from "./controllers/certifications.controller";
 import conversationsController from '@/controllers/conversations.controller';
+import usersController from '@/controllers/users.controller';
 
 const router: ExpressRouter = Router();
 
@@ -19,5 +20,6 @@ router.use("/notifications", notificationsController);
 router.use("/auth-logs", authLogsController);
 router.use('/certifications', certificationsController);
 router.use('/conversations', conversationsController);
+router.use('/users', usersController);
 
 export default router;
