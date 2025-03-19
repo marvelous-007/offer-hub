93dcd551188612875372bfd1b7c7d0fb5710798b
Josue19-08 <josuemarin2009@hotmail.com>
2025-03-19 10:00:51 -0600

    feat: add DTOs for conversation-participants module

--- backend/src/modules-v2/conversation-participants/dto.ts
+++ backend/src/modules-v2/conversation-participants/dto.ts
@@ -1,1 +1,9 @@
-// Here dto.ts
+import { IsUUID } from 'class-validator';
+
+export class CreateConversationParticipantDto {
+  @IsUUID()
+  conversation_id: string;
+
+  @IsUUID()
+  user_id: string;
+}
