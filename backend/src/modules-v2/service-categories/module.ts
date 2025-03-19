b43d313f18d9223e2bbeb86635be776ed08d5268
Josue19-08 <josuemarin2009@hotmail.com>
2025-03-19 10:00:54 -0600

    feat: create service-categories NestJS module

---
+++ backend/src/modules-v2/service-categories/module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServiceCategoriesController } from './controller';
+import { ServiceCategoriesService } from './service';
+import { ServiceCategory } from './entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ServiceCategory])],
+  controllers: [ServiceCategoriesController],
+  providers: [ServiceCategoriesService],
+  exports: [ServiceCategoriesService],
+})
+export class ServiceCategoriesModule {}
