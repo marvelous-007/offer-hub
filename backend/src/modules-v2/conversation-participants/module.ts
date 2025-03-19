646deee2845220876699187d331c717f8ca4fe9f
Josue19-08 <josuemarin2009@hotmail.com>
2025-03-19 10:00:52 -0600

    feat: create conversation-participants NestJS module

--- backend/src/modules-v2/conversation-participants/module.ts
+++ backend/src/modules-v2/conversation-participants/module.ts
@@ -1,1 +1,13 @@
-// Here module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConversationParticipantsController } from './controller';
+import { ConversationParticipantsService } from './service';
+import { ConversationParticipant } from './entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ConversationParticipant])],
+  controllers: [ConversationParticipantsController],
+  providers: [ConversationParticipantsService],
+  exports: [ConversationParticipantsService],
+})
+export class ConversationParticipantsModule {}
