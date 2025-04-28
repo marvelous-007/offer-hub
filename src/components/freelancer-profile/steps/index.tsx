2d4eb189e838ad9b7269dd7a7eb09db9c37080e5
Billy <tobiikupolati33@gmail.com>
2025-04-28 23:34:23 +0100

    feat: implement add service rate step for the onboarding

--- src/components/freelancer-profile/steps/index.tsx
+++ src/components/freelancer-profile/steps/index.tsx
@@ -1,34 +1,44 @@
-'use client'
+'use client';
 
-import { useFreelancerSteps } from '@/hooks/use-freelancer-steps'
+import { useFreelancerSteps } from '@/hooks/use-freelancer-steps';
 import UserAddWorkExperience from '@/components/freelancer-profile/steps/user-add-work-experience';
+import UserSetHourlyRateActiveState from './user-set-hourly-rate-active-state';
 
 const steps = [
   { key: 'user-choose-role', component: null }, // to be implemented
   { key: 'user-select-job-type', component: null }, // to be implemented
-  { key: 'user-select-interested-category', component: null },  // to be implemented
+  { key: 'user-select-interested-category', component: null }, // to be implemented
   { key: 'user-add-work-experience', component: <UserAddWorkExperience /> },
-  { key: 'user-add-work-experience-active-state', component: null },  // to be implemented
-  { key: 'user-add-work-experience-active-state-not-in-focus', component: null },  // to be implemented
-  { key: 'user-add-work-experience-default-state', component: null },  // to be implemented
-  { key: 'user-add-education-default-state', component: null },  // to be implemented
-  { key: 'user-choose-languaje-active-state', component: null },  // to be implemented
-  { key: 'user-write-bio', component: null },  // to be implemented
-  { key: 'user-enter-service-fee', component: null },  // to be implemented
-  { key: 'user-setting-up-account-profile-active-state', component: null },  // to be implemented
-  { key: 'user-profile-photo-active-and-focus-state', component: null },  // to be implemented
-  { key: 'user-profile-set-uo-preview', component: null },  // to be implemented
-]
+  { key: 'user-add-work-experience-active-state', component: null }, // to be implemented
+  {
+    key: 'user-add-work-experience-active-state-not-in-focus',
+    component: null,
+  }, // to be implemented
+  { key: 'user-add-work-experience-default-state', component: null }, // to be implemented
+  { key: 'user-add-education-default-state', component: null }, // to be implemented
+  {
+    key: 'user-set-hourly-rate-active-state',
+    component: <UserSetHourlyRateActiveState />,
+  },
+  { key: 'user-set-hourly-rate-active-state-not-in-focus', component: null }, // to be implemented
+  { key: 'user-set-hourly-rate-default-state', component: null }, // to be implemented
+  { key: 'user-choose-languaje-active-state', component: null }, // to be implemented
+  { key: 'user-write-bio', component: null }, // to be implemented
+  { key: 'user-enter-service-fee', component: null }, // to be implemented
+  { key: 'user-setting-up-account-profile-active-state', component: null }, // to be implemented
+  { key: 'user-profile-photo-active-and-focus-state', component: null }, // to be implemented
+  { key: 'user-profile-set-uo-preview', component: null }, // to be implemented
+];
 
 export default function StepsController() {
-  const { currentStep, nextStep, prevStep } = useFreelancerSteps()
-  const StepComponent = steps[currentStep]?.component
+  const { currentStep, nextStep, prevStep } = useFreelancerSteps();
+  const StepComponent = steps[currentStep]?.component;
 
   return (
     <>
       {StepComponent ? StepComponent : <p>This step is not yet implemented.</p>}
 
-      <div className="mt-8 flex justify-between">
+      <div className='mt-8 flex justify-between'>
         <button onClick={prevStep} disabled={currentStep === 0}>
           Back
         </button>
@@ -37,5 +47,5 @@ export default function StepsController() {
         </button>
       </div>
     </>
-  )
+  );
 }
