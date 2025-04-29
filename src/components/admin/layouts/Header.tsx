52129ac8c68a0fb5d6261a313279231e261e3945
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2025-04-28 18:55:00 -0600

    feat: Update profile image

--- src/components/admin/layouts/Header.tsx
+++ src/components/admin/layouts/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="flex items-center justify-between border-b p-6">
       <h2 className="text-xl font-medium">Good Morning John Doe</h2>
       <div className="h-10 w-10 overflow-hidden rounded-full bg-gray-200">
-        <Image src="/example.png" alt="User avatar" width={40} height={40} />
+        <Image src="/profile.jpeg" alt="User avatar" width={40} height={40} />
       </div>
     </div>
   );
