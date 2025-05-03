542353cb83635b962b6bf4d22360899383b5f301
Michelle031 <mbaangel2803@gmail.com>
2025-05-03 15:50:29 +0100

    resolving conflicts

--- src/components/freelancer-profile/steps/index.tsx
+++ src/components/freelancer-profile/steps/index.tsx
@@ -1,35 +1,47 @@
-'use client'
-
-import { useFreelancerSteps } from '@/hooks/use-freelancer-steps'
-import UserAddLanguagesActiveState from './user-add-languages-active-state'
-import UserSelectJobType from './user-select-job-type'
+'use client';
+import { useFreelancerSteps } from '@/hooks/use-freelancer-steps';
+import UserAddWorkExperienceDefaultState from './user-add-work-experience-default-state';
+import UserSetHourlyRateActiveState from './user-set-hourly-rate-active-state';
+import UserSelectJobType from './user-select-job-type';
+import Header from '../header';
 
 const steps = [
   { key: 'user-choose-role', component: null }, // to be implemented
-  { key: 'user-select-job-type', component: <UserSelectJobType /> }, // to be implemented
-  { key: 'user-select-interested-category', component: null },  // to be implemented
-  { key: 'user-add-work-experience', component: null },  // to be implemented
-  { key: 'user-add-work-experience-active-state', component: null },  // to be implemented
-  { key: 'user-add-work-experience-active-state-not-in-focus', component: null },  // to be implemented
-  { key: 'user-add-work-experience-default-state', component: null },  // to be implemented
-  { key: 'user-add-education-default-state', component: null },  // to be implemented
-  { key: 'user-choose-language-active-state', component: <UserAddLanguagesActiveState /> },  // to be implemented
-  { key: 'user-write-bio', component: null },  // to be implemented
-  { key: 'user-enter-service-fee', component: null },  // to be implemented
-  { key: 'user-setting-up-account-profile-active-state', component: null },  // to be implemented
-  { key: 'user-profile-photo-active-and-focus-state', component: null },  // to be implemented
-  { key: 'user-profile-set-uo-preview', component: null },  // to be implemented
-]
+  { key: 'user-select-job-type', component: UserSelectJobType },
+  { key: 'user-select-interested-category', component: null }, // to be implemented
+  { key: 'user-add-work-experience', component: null }, // to be implemented
+  { key: 'user-add-work-experience-active-state', component: null }, // to be implemented
+  {
+    key: 'user-add-work-experience-active-state-not-in-focus',
+    component: null,
+  }, // to be implemented
+  {
+    key: 'user-add-work-experience-default-state',
+    component: <UserAddWorkExperienceDefaultState />,
+  },
+
+  { key: 'user-add-education-default-state', component: null }, // to be implemented
+  {
+    key: 'user-set-hourly-rate-active-state',
+    component: <UserSetHourlyRateActiveState />,
+  },
+  { key: 'user-choose-languaje-active-state', component: null }, // to be implemented
+  { key: 'user-write-bio', component: null }, // to be implemented
+  { key: 'user-enter-service-fee', component: null }, // to be implemented
+  { key: 'user-setting-up-account-profile-active-state', component: null }, // to be implemented
+  { key: 'user-profile-photo-active-and-focus-state', component: null }, // to be implemented
+  { key: 'user-profile-set-uo-preview', component: null }, // to be implemented
+];
 
 export default function StepsController() {
   const { currentStep, nextStep, prevStep } = useFreelancerSteps()
   const StepComponent = steps[currentStep]?.component
 
   return (
-    <>
-      {StepComponent ? StepComponent : <p>This step is not yet implemented.</p>}
+      <section className='flex flex-col gap-y-16 min-h-svh'>
+      <Header />
 
-      <div className="mt-8 flex justify-between">
+      <div className='mt-8 flex justify-between'>
         <button onClick={prevStep} disabled={currentStep === 0}>
           Back
         </button>
@@ -37,6 +49,18 @@ export default function StepsController() {
           Next
         </button>
       </div>
-    </>
-  )
+      <div className='flex-1 flex'>
+        {StepComponent ? (
+          typeof StepComponent === 'function' ? (
+            <StepComponent />
+          ) : (
+            StepComponent
+          )
+        ) : (
+          <p>This step is not yet implemented.</p>
+        )}
+      </div>
+      {/* <UserAddWorkExperienceDefaultState /> */}
+    </section>
+  );
 }
