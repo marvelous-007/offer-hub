e5b9dfa132ccbc2ae512bc98b2250cc590312097
obiabo <54102389+yhoungdev@users.noreply.github.com>
2025-07-01 01:46:15 +0100

    chore(ui): fixed UI descrepancy on dashboard-resolution path

--- src/app/admin/dispute-resolution/layout.tsx
+++ src/app/admin/dispute-resolution/layout.tsx
@@ -2,7 +2,7 @@
 import type { ReactNode  } from "react";
 
 export default function DisputeResolutionLayout({ children }: { children: ReactNode }) {
-  return <div className="min-h-screen bg-white pt-5">
+  return <div className="min-h-screen  pt-5">
       {children}
   </div>;
 } 
--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,10 +4,11 @@ import type React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen bg-white">
+    <div className="flex h-screen ">
       <Sidebar />
-      <div className="flex-1 flex flex-col overflow-auto">
+      <div className="flex-1 flex flex-col overflow-auto bg-[#F6F6F6]">
         <Header />
+        
         <main className="flex-1">{children}</main>
       </div>
     </div>
