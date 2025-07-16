34fa482addc08d8c7e90cd288df2644f46465e97
Josue19-08 <josuemarin2009@hotmail.com>
2025-07-16 14:41:59 -0600

    refactor: remove pages

---
+++ src/app/help/page.tsx
@@ -0,0 +1,16 @@
+import HelpHeader from "../../components/help/help-header";
+import HelpCategoryCards from "../../components/help/help-category-cards";
+import HelpTopicsTabs from "@/components/help/help-topics-taps";
+import ContactSupport from "@/components/help/contact-support";
+import CommunityResources from "@/components/help/community-resources";
+export default function HelpPage() {
+  return (
+    <>
+      <HelpHeader />
+      <HelpCategoryCards />
+      <HelpTopicsTabs />
+      <ContactSupport />
+      <CommunityResources />
+    </>
+  );
+}
