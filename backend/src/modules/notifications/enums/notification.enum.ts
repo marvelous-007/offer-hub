f27a3e229fcc6c3438f928153cd4545caa257644
Josue19-08 <josuemarin2009@hotmail.com>
2025-07-16 15:55:59 -0600

    delete: notification.enum.ts

--- backend/src/modules/notifications/enums/notification.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum NotificationType {
-  GENERAL = "general",
-  VERIFICATION_SUBMITTED = "verification_submitted",
-  VERIFICATION_APPROVED = "verification_approved",
-  VERIFICATION_REJECTED = "verification_rejected",
-}
-
