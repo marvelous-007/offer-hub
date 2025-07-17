0ce4863fda67f2c4d8d47d1213cc6da408d57064
Kevin Brenes <kevinbrenes@Kevins-MacBook-Air.local>
2025-07-16 18:18:52 -0600

    feat(routing): add new dispute resolution routes structure

---
+++ src/app/(admin-dispute)/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/admin/layouts/Header";
+import Sidebar from "@/components/admin/layouts/Sidebar";
+import type React from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen ">
+      <Sidebar />
+      <div className="flex-1 flex flex-col overflow-auto bg-[#F6F6F6]">
+        <Header />
+        
+        <main className="flex-1">{children}</main>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(admin-dispute)/platform-monitoring/layout.tsx
@@ -0,0 +1,8 @@
+'use client'
+import type { ReactNode  } from "react";
+
+export default function DisputeResolutionLayout({ children }: { children: ReactNode }) {
+  return <div className="min-h-screen  pt-5">
+      {children}
+  </div>;
+} 
