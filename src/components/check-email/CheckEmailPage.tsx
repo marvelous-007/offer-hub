1bedc8215c7299dc3d0e88e4efe03121a89aeea7
JoelVR17 <vermudo.com@gmail.com>
2025-07-23 12:27:01 -0600

    feat: add CheckEmailPage component to render email confirmation during onboarding

---
+++ src/components/check-email/CheckEmailPage.tsx
@@ -0,0 +1,11 @@
+import LoginHeader from "../login/LoginHeader";
+import { CheckEmail } from "./CheckEmail";
+
+export const CheckEmailPage = () => {
+  return (
+    <div>
+      <LoginHeader />
+      <CheckEmail />
+    </div>
+  );
+};
