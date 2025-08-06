7bd5941b4a02b5e9c4386ab1d01df0671781297e
Joel Vargas <vermudo.com@gmail.com>
2025-08-06 12:09:12 -0600

    feat: create ActiveProjectPage component for onboarding dashboard

---
+++ src/app/(client)/onboarding/dashboard/active-project/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { ActiveProjectManagement } from "@/components/client-dashboard/ActiveProjectManagement";
+
+export default function ActiveProjectPage() {
+  return (
+    <>
+      <ActiveProjectManagement />
+    </>
+  );
+}
