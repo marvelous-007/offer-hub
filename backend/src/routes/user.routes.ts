84c26c91f91d4b6c71c4e727224797320ee3ca0f
utnim2 <mintugogoi567@gmail.com>
2025-08-15 18:03:24 +0530

    feat(backend): add users endopoint

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import {
   createUserHandler,
   getUserByIdHandler,
-  updateUserHandler
+  updateUserHandler,
+  getAllUsersHandler
 } from '@/controllers/user.controller';
 
 const router = Router();
 
+router.get('/', getAllUsersHandler);
 router.post('/', createUserHandler);
 router.get('/:id', getUserByIdHandler);
 router.patch('/:id', updateUserHandler);
